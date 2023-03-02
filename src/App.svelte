c0c743800228a0c6b3ffac81bb1953f67695a4d5
Warren Wu <76506508+VVarren@users.noreply.github.com>
2023-03-02 13:29:19 -0500

    big changes

--- src/App.svelte
+++ src/App.svelte
@@ -1,46 +1,41 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+	import svelteLogo from "./assets/svelte.svg";
+	import Card from "./lib/Card.svelte";
+	let front: string = "";
+	let back: string = "";
+	interface ICard {
+		front: string;
+		back: string;
+	}
+	let cards: ICard[] = [];
+	const AddCard = () => {
+		if (front != "" && back != "") {
+			cards = [
+				...cards,
+				{
+					front: front,
+					back: back,
+				},
+			];
+			front = "";
+			back = "";
+		}
+	};
+	/*To add 1. Home page with button to makea set 2. Sets hold cards 3. Cards can
+be edited in the set */
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+	<div>
+		<input bind:value={front} placeholder="Front" class="FrontOfCard" />
+		<input bind:value={back} placeholder="Back" class="BackOfCard" />
+		<button on:click={AddCard}>Add me :) </button>
 
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+		{#each cards as card}
+			<Card front={card.front} back={card.back} />
+		{/each}
+	</div>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
 </style>
---
+++ src/lib/Card.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	export let front: string;
+	export let back: string;
+	let count: number = 0;
+	let current: string = front;
+	let isFront: boolean = true;
+	let SideofCard: string = "Front of Card";
+
+	const ChangeCard = () => {
+		isFront = !isFront;
+		isFront ? (current = front) : (current = back);
+		isFront ? (SideofCard = "Front of Card") : (SideofCard = "Back of Card");
+	};
+</script>
+
+<button class="Card" on:click={ChangeCard}>
+	*Current side of card is {SideofCard}* {current}
+</button>
