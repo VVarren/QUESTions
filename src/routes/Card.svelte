2d166471e13a2c74b492fffe96dabd3229b4ac69
Warren Wu <76506508+VVarren@users.noreply.github.com>
2023-06-14 12:22:56 -0400

    some big progress guys

---
+++ src/routes/Card.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	
+</script>
+
+<div class = cardContainer>
+		<div class = cardTitle>Card Title</div>
+		<div class = cardContent>Card Contents Card Contents Card Contents Card Contents Card Contents Card Contents </div>
+</div>
+<style>
+.cardContainer{
+		background-color:red;
+		display: flex;
+		flex-direction: column;
+		width:50%;
+		height:30%;
+	}
+	.cardTitle{
+		background-color:blue;
+		
+	}
+	.cardContent{
+		background-color:green;
+		
+	}
+</style>
