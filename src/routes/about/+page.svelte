c84ab5afca3b1a941cf50db3c873c40ab35bc5b6
Warren Wu <76506508+VVarren@users.noreply.github.com>
2023-06-15 17:47:56 -0400

    more changes tbh i forgot what i did.

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -5,11 +5,40 @@
 
 <h1>Create A Card</h1>
 <div class = "inputContainer">
-<input bind:value={title} placeholder="Enter a title:"/>
-<input bind:value={backContent} placeholder="Enter the back content:"/>
+<textarea bind:value={title} class = "titleContainer" placeholder="Enter a title:"/>
+<textarea bind:value={backContent} class = "backContentContainer" placeholder="Enter the back content:"/>
 </div>
+
+
 <style>
  .inputContainer{
-	background-color:blue;
+	border-style: solid;
+	border-radius: 20px;
+	justify-content: center;
+	align-items: center;
+	display: flex;
+	flex-direction: column;
+	box-shadow: 5px 10px;
+
+ }
+
+ .titleContainer{
+	margin-top: 3%;
+	margin-bottom: 1.5%;
+	border-radius: 20px;
+	border-width: 2px;
+	width: 50vh;
+	text-align: center;
+
+}
+ .backContentContainer{
+	margin-top: 1.5%;
+	margin-bottom: 3%;
+	border-radius: 20px;
+	width: 50vh;
+	height:50vh;
+	border-width: 2px;
+	text-align: center;
+
  }
 </style>
